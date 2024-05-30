60c7242ec40ac9136e6cc70e77ffda91563db989
tomarpraveengithub460 <praveentomar460@gmail.com>
2024-05-30 08:54:53 +0530

    Adding files

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement("h1", {}, "Hello World Using React via CDN");
 const Root = ReactDOM.createRoot(document.getElementById("root"));
 Root.render(heading);
 
 
 // Way to create Nested Elements
-const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "Hello1"), 
-        React.createElement("h1", {}, "Hello2")
-    ]),
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "Hello3"), 
-        React.createElement("h1", {}, "Hello4")
-    ])
-]);
-const root = ReactDOM.createRoot(document.getElementsByTagName("body"));
-root.render(parent);
+// const parent = React.createElement("div", { id: "parent" }, [
+//     React.createElement("div", { id: "child1" }, [
+//         React.createElement("h1", {}, "Hello1"), 
+//         React.createElement("h1", {}, "Hello2")
+//     ]),
+//     React.createElement("div", { id: "child2" }, [
+//         React.createElement("h1", {}, "Hello3"), 
+//         React.createElement("h1", {}, "Hello4")
+//     ])
+// ]);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
